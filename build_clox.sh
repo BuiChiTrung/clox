0e63ed0b2866e1c47698dccc5180074b8318ccfb
Bui Chi Trung <trungcspntl@gmail.com>
2025-01-27 17:06:47 +0700

    update README.md

--- build_clox.sh
+++ build_clox.sh
@@ -1,2 +1,6 @@
+# Install dependencies
 git submodule add -f https://github.com/google/googletest.git third_party/googletest
-git submodule update --init --recursive
+
+# Build the code with Cmake
+cmake -B ./build
+cmake --build ./build
